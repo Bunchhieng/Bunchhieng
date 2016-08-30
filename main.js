ad5fc90fdf9d575370cc1cda78cdc2f2e59adbdb
Bunchhieng <Bunchhieng@gmail.com>
2016-08-29 23:38:11 -0400

    Still trying to make SPA works

--- main.js
+++ main.js
@@ -4,14 +4,19 @@ import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import App from './src/components/App';
 import Courses from './src/components/Courses';
 import Home from './src/components/home';
+import PageNotFound from './src/components/PageNotFound';
 import Projects from './src/components/Projects';
 
+const routes = (
+  <Route path="/" mapMenuTitle="Home" component={App}>
+    <IndexRoute component={Home} />
+    <Route path="/courses" mapMenuTitle="Coures" component={Courses} />
+    <Route path="/projects" mapMenuTitle="Projects" component={Projects} />
+
+    <Route path="*" mapMenuTitle="Page Not Found" component={PageNotFound} />
+  </Route>
+);
+
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Home}/>
-      <Route path="/courses" component={Courses} />
-      <Route path="/projects" component={Projects} />
-    </Route>
-  </Router>
-), document.getElementById('app'))
+  <Router history={browserHistory} routes={routes}/>
+), document.getElementById('app'));
