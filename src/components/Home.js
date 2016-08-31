546bfbf4aa5e34c4e66747ddea222b00a9376017
Bunchhieng <Bunchhieng@gmail.com>
2016-08-31 19:58:53 -0400

    misc

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom';
 export default class Home extends React.Component {
   render() {
     return (
-      <div>
+      <div className="list">
         <h4>Home page</h4>
-        <p>Welcome to site...</p>
+        <p>SPA on github page :P...</p>
       </div>
     )
   }
